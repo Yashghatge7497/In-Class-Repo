44f4a21bb4f17a0253ab4b41e122b10f3701464a
Yashghatge7497 <yashghatge123@gmail.com>
2022-03-10 18:53:40 -0500

    debug chnages

--- client/src/models/session.ts
+++ client/src/models/session.ts
@@ -1,25 +1,47 @@
-
-import router from "../router";
-import * as users from "../models/user";
 import { reactive } from "vue";
+import router from "../router";
+
+import * as users from "../models/user";
+import { useMessages } from "./messages";
+
 
 const session = reactive({
     user: null as users.User | null,
+    destinationUrl: null as string | null,
 })
 
 export async function Login(handle: string, password: string) {
-    const user = users.list.find(u=>u.handle === handle );
-    if(!user){
+    const user = users.list.find(u => u.handle === handle);
+    const messages = useMessages();
 
-        throw {message: "user not found"};
-        
-    }
-    if(user.password !== password){
+    try {
+        if (!user) {
+            throw { message: "User not found" };
+        }
+        if(user.password !== password) {
+            throw { message: "Incorrect password" };
+        } 
 
-        throw {message: "Incorrect Password"};
+        messages.notifications.push({
+            type: "success",
+            message: 'Welcome back ${user.firstName}!',
+        });
+
+        session.user = user;
+        router.push(session.destinationUrl  ?? '/wall');
+
+    } catch (error: any) {
+        messages.notifications.push({
+            type: "danger",
+            message: error.message,
+        });
+        console.table(messages.notifications)
     }
-    session.user = user;
-    router.push('/messages');
+}
+
+export function Logout() {
+    session.user = null;
+    router.push('/login');
 }
     
 export default session;

9594572ced2bfb588576bc579273b739911c551b
Yashghatge7497 <98485747+Yashghatge7497@users.noreply.github.com>
2022-03-11 00:48:35 -0500

    Update Messages.vue

--- ToDo/client/src/components/Messages.vue
+++ ToDo/client/src/components/Messages.vue
@@ -5,13 +5,13 @@
 
 <template>
     <div>
-        <div v-for=" (x, i) in messages.notifications" :class="`notification is-${x.type}`">
+        <div v-for=" (x, i) in messages.notifications" :class="'notification is-${x.type}'">
             <button class="delete" @click="messages.close(i)" ></button>
             {{ x.message }}
         </div>
     </div>
 </template>
-
+<!-- -->
 
 <style scoped>
 </style>
